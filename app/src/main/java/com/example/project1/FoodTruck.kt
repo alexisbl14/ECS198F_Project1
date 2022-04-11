38baaa5d796ed479a30237ec0f91446ecc95b4f8
alexisbl14 <ablydon@ucdavis.edu>
2022-04-10 17:18:02 -0700

    phase 1.1 and 1.2 complete

---
+++ app/src/main/java/com/example/project1/FoodTruck.kt
@@ -0,0 +1,11 @@
+package com.example.project1
+
+data class FoodTruck(
+    var ID: Int,
+    var foodTruckName: String,
+    var foodTruckImage: Int,
+    var location: String,
+    var timeOpen: String,
+    var foodTruckDescription: String,
+    var website: String
+)
